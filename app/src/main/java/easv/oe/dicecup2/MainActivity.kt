c8b861e1448e1fb0ddec4cd429399d9320e3e367
oleeriksen <oe@easv.dk>
2022-01-29 16:25:11 +0100

    Initial commit

---
+++ app/src/main/java/easv/oe/dicecup2/MainActivity.kt
@@ -0,0 +1,47 @@
+package easv.oe.dicecup2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val TAG: String = "xyz"
+
+    // mapping from 1..6 to drawables, the first index is unused
+    private val diceId = intArrayOf(0, R.drawable.dice1,
+        R.drawable.dice2,
+        R.drawable.dice3,
+        R.drawable.dice4,
+        R.drawable.dice5,
+        R.drawable.dice6)
+
+    private val mRandomGenerator = Random()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnRoll.setOnClickListener { v -> onClickRoll() }
+        Log.d(TAG, "OnCreate")
+
+    }
+
+    private fun onClickRoll(){
+        val e1 = mRandomGenerator.nextInt(6) + 1
+        val e2 = mRandomGenerator.nextInt(6) + 1
+
+        // set dices
+        updateDicesWith(e1, e2)
+        Log.d(TAG, "Roll")
+    }
+
+    private fun updateDicesWith(d1: Int, d2: Int) {
+        imgDice1.setImageResource( diceId[d1] )
+        imgDice2.setImageResource( diceId[d2] )
+    }
+
+
+
+}
