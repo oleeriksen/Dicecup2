7fc1925f3ff931cf4fdb76347dee6a94bad307e9
ceci6182 <ceci6182@easv365.dk>
2022-02-03 14:19:16 +0100

    Update StoryActivity.kt

--- app/src/main/java/easv/oe/dicecup2/StoryActivity.kt
+++ app/src/main/java/easv/oe/dicecup2/StoryActivity.kt
@@ -57,7 +57,7 @@ class StoryActivity : BasicActivity() {
 
         val diceId = diceManager.diceImages
 
-        val allDices = listOf(view.dice1, view.dice2, view.dice3, view.dice4, view.dice4, view.dice5, view.dice6, view.dice7, view.dice8, view.dice9)
+        val allDices = listOf(view.dice1, view.dice2, view.dice3, view.dice4, view.dice5, view.dice6, view.dice7, view.dice8, view.dice9)
 
         view.txtRollText.setText("Amount of rolled dice: " + amount)
         view.txtRollText.setTypeface(null, Typeface.BOLD)
@@ -68,68 +68,13 @@ class StoryActivity : BasicActivity() {
         for(dice in allDices){
             if(i<amount){
                 dice.visibility = View.VISIBLE
+                allDices[i].setImageResource(diceId[array[i]])
             }
-
             else{
                 dice.visibility = View.GONE
             }
+            i++
         }
-
-
-        /*
-        when(amount){
-            2-> d2.visibility= View.VISIBLE
-            3-> {d2.visibility= View.VISIBLE
-                d3.visibility= View.VISIBLE}
-            4-> {d2.visibility= View.VISIBLE
-                d3.visibility= View.VISIBLE
-                d4.visibility= View.VISIBLE}
-            5-> {d2.visibility= View.VISIBLE
-                d3.visibility= View.VISIBLE
-                d4.visibility= View.VISIBLE
-                d5.visibility= View.VISIBLE}
-            6-> {d2.visibility= View.VISIBLE
-                d3.visibility= View.VISIBLE
-                d4.visibility= View.VISIBLE
-                d5.visibility= View.VISIBLE
-                d6.visibility= View.VISIBLE}
-            7-> {d2.visibility= View.VISIBLE
-                d3.visibility= View.VISIBLE
-                d4.visibility= View.VISIBLE
-                d5.visibility= View.VISIBLE
-                d6.visibility= View.VISIBLE
-                d7.visibility= View.VISIBLE}
-            8-> {d2.visibility= View.VISIBLE
-                d3.visibility= View.VISIBLE
-                d4.visibility= View.VISIBLE
-                d5.visibility= View.VISIBLE
-                d6.visibility= View.VISIBLE
-                d7.visibility= View.VISIBLE
-                d8.visibility= View.VISIBLE}
-            9-> {d2.visibility= View.VISIBLE
-                d3.visibility= View.VISIBLE
-                d4.visibility= View.VISIBLE
-                d5.visibility= View.VISIBLE
-                d6.visibility= View.VISIBLE
-                d7.visibility= View.VISIBLE
-                d8.visibility= View.VISIBLE
-                d9.visibility= View.VISIBLE}
-            else -> {d2.visibility= View.GONE
-                d3.visibility= View.GONE
-                d4.visibility= View.GONE
-                d5.visibility= View.GONE
-                d6.visibility= View.GONE
-                d7.visibility= View.GONE
-                d8.visibility= View.GONE
-                d9.visibility= View.GONE}
-        }
-        
-         */
-
-        for (i in (0 until amount)) {
-            allDices[i].setImageResource(diceId[array[i]])
-        }
-
 
         container.addView(view)
     }
