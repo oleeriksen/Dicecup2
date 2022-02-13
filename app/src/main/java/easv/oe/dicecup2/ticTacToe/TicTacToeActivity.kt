a45e6d8e6be14ff1f21ec62cac2ed023275e006d
ceci6182 <ceci6182@easv365.dk>
2022-02-13 22:03:04 +0100

    start

---
+++ app/src/main/java/easv/oe/dicecup2/ticTacToe/TicTacToeActivity.kt
@@ -0,0 +1,12 @@
+package easv.oe.dicecup2.ticTacToe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import easv.oe.dicecup2.R
+
+class TicTacToeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tic_tac_toe)
+    }
+}
